2a993ee4052d226a509e5576921cbf2a409683b8
jamong13 <limehs0618@gmail.com>
2025-11-20 18:18:46 +0900

    a1120

--- react/a_portfolio/personal_page/src/components/Intro.jsx
+++ react/a_portfolio/personal_page/src/components/Intro.jsx
@@ -5,12 +5,31 @@ import { faArrowPointer } from "@fortawesome/free-solid-svg-icons";
 
 export default function Intro() {
   return (
-    <div className="intro">
+    <div id="intro" className="jersey-25-regular">
       <div className="container intro-container">
-        <div className="intro-txt intro-txt1"></div>
-        <div className="intro-txt intro-txt2"></div>
-        <div className="intro-txt intro-txt3"></div>
-        <FontAwesomeIcon icon={faArrowPointer} />
+        <div className="intro-txt intro-txt1">
+          <span>Who</span>
+        </div>
+        <div className="intro-txt intro-txt2">
+          <span>Is</span>
+        </div>
+        <div className="intro-txt intro-txt3">
+          <span>LEE HEE SOO</span>
+        </div>
+        <div className="intro-txt intro-txt4">
+          <span>?</span>
+        </div>
+        <div className="intro-mark">
+          <div className="circle-box big-circle">
+            <div className="circle-box medium-circle">
+              <div className="circle-box small-circle"></div>
+            </div>
+          </div>
+          <div className="intro-mark-question">
+            <FontAwesomeIcon icon={faArrowPointer} />
+          </div>
+          <p>Click</p>
+        </div>
       </div>
     </div>
   );
--- react/slick/src/App.jsx
+++ react/slick/src/App.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import "./App.css";
 import BasicSlider from "./components/BasicSlider";
+import AutoplaySlider from "./components/AutoplaySlider";
+import FadeSlider from "./components/FadeSlider";
+import MultipleSlider from "./components/MultipleSlider";
+import ThumbnailSlider from "./components/ThumbnailSlider";
+import ResponsiveSlider from "./components/ResposiveSlider";
+import CustomNavSlider from "./components/CustomNavSlider";
+import FontAwesomeSlider from "./components/FontAwesomeSlider";
+import ImageArrowSlider from "./components/ImageArrowSlider";
 
 export default function App() {
   return (
@@ -9,6 +17,38 @@ export default function App() {
       <section className="slider-section">
         <h2>1. 기본 슬라이더(Basic Slider)</h2>
         <BasicSlider />
+      </section>
+      <section className="slider-section">
+        <h2>2. 자동 재생 슬라이더(Autoplay Slider)</h2>
+        <AutoplaySlider />
+      </section>
+      <section className="slider-section">
+        <h2>3. 페이드 슬라이더(Fade Slider)</h2>
+        <FadeSlider />
+      </section>
+      <section className="slider-section">
+        <h2>4. 다중 슬라이드 슬라이더(Multiple Slider)</h2>
+        <MultipleSlider />
+      </section>
+      <section className="slider-section">
+        <h2>5. 썸네일 네비게이션 슬라이더 (Thumbnail Navigation Slider)</h2>
+        <ThumbnailSlider />
+      </section>
+      <section className="slider-section">
+        <h2>6. 반응형 네비게이션 슬라이더 (Responsive Slider)</h2>
+        <ResponsiveSlider />
+      </section>
+      <section className="slider-section">
+        <h2>7. 커스텀 네비 네비게이션 슬라이더 (Custom Nav Slider)</h2>
+        <CustomNavSlider />
+      </section>
+      <section className="slider-section">
+        <h2>8. 커스텀 네비 네비게이션 슬라이더 (Font-awesome Slider)</h2>
+        <FontAwesomeSlider />
+      </section>
+      <section className="slider-section">
+        <h2>9. 커스텀 네비 네비게이션 슬라이더 (Image Arrow Slider)</h2>
+        <ImageArrowSlider />
       </section>
     </div>
   );
